31ca741408641796bff7bb07d1951b583804bf26
Bapi Mondal <bapi@Bapis-MacBook-Air.local>
2026-06-26 14:19:48 +0530

    fix: suppress harmless unexpected_cfgs warnings in smart contracts

--- contracts/campaign/src/lib.rs
+++ contracts/campaign/src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![allow(unexpected_cfgs)]
 
 use soroban_sdk::{contract, contractimpl, contracttype, token, Address, Env};
 
--- contracts/factory/src/lib.rs
+++ contracts/factory/src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![allow(unexpected_cfgs)]
 
 use soroban_sdk::{contract, contractimpl, contracttype, Address, BytesN, Env, Vec};
 
